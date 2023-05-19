084e37d986624c9449086bec2f21162eee9d58c3
Annataliaa <annatalia66842@gmail.com>
2023-05-19 22:25:16 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Laravel</title>
+</head>
+<body>
+    <div class="container">
+        <table class="table mt-5">
+            <thead>
+              <tr class="bg-primary">
+                <th scope="col">ID</th>
+                <th scope="col">Aksi</th>
+                <th scope="col">Avatar</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/fotoku.jpg" alt="" width="100px"></td>
+                <td>Annatalia</td>
+                <td>Annatalia@gmail.com</td>
+                <td>085842823380</td>
+                <td>Staf</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/Vanya.jpg" alt="" width="100px"></td>
+                <td>Vanya Radjwa</td>
+                <td>VanyaJKT@gmail.com</td>
+                <td>08199965143</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/Reza.jpg" alt="" width="100px"></td>
+                <td>Reza Irvando</td>
+                <td>Reza123@gmail.com</td>
+                <td>081587744565</td>
+                <td>Staf</td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/Jian.jpg" alt="" width="100px"></td>
+                <td>Jian Lombok</td>
+                <td>JianLombok@gmail.com</td>
+                <td>089874485665</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+                <th scope="row">5</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/Rayhan.jpg" alt="" width="100px"></td>
+                <td>Rayhan San</td>
+                <td>Sanrayhan@gmail.com</td>
+                <td>089874485665</td>
+                <td>Staf</td>
+              </tr>
+              <tr>
+                <th scope="row">6</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/sandy.jpg" alt="" width="100px"></td>
+                <td>Sandhy Hruanda</td>
+                <td>Sandy@gmail.com</td>
+                <td>089447568885</td>
+                <td>Staf</td>
+              </tr>
+              <tr>
+                <th scope="row">7</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/Adel.jpg" alt="" width="100px"></td>
+                <td>Adela Nilal </td>
+                <td>Adelaa@gmail.com</td>
+                <td>0888795558</td>
+                <td>Staf</td>
+              </tr>
+              <tr>
+                <th scope="row">8</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/anastasia.jpg" alt="" width="100px"></td>
+                <td>Anastasia</td>
+                <td>Anastasia@gmail.com</td>
+                <td>089447568885</td>
+                <td>Admin</td>
+              </tr>
+              <tr>
+                <th scope="row">9</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="/img/Leny.jpg" alt="" width="100px"></td>
+                <td>Leny, Nata, Kirana</td>
+                <td>lenatkir@gmail.com</td>
+                <td>089447568885</td>
+                <td>Admin</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+</body>
+</html>
