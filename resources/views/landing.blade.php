490ddb1a0ea6eba587aa4dacfa2a8f701522bb18
Annataliaa <annatalia66842@gmail.com>
2023-06-10 20:53:31 +0700

    p28

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -95,7 +95,7 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2023 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">Annatalia (cupcake & cake)</a>
+      <a class="text-white" href="https://mdbootstrap.com/">Annatalia (cupcake&cake)</a>
     </div>
     <!-- Copyright -->
   </footer>
